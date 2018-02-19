43ed978618ba9ed1617e0eaca1d55acf889c8576
丁锐 <dingrui@daojia.com>
2018-02-19 22:37:05 +0800

    修改table表名key

--- app/model/{{ModelName}}Model.js
+++ app/model/{{ModelName}}Model.js
@@ -1,5 +1,4 @@
 const db = require('../helper/db')
 
 module.exports = db.Model.extend({
-      tableName: '{{table.name}}',
 })
--- app/router/{{ModelName}}Router.js
+++ app/router/{{ModelName}}Router.js
@@ -1,16 +1,3 @@
 const router = require('koa-router')()
-const controller = require('../controller/{{ModelName}}Controller')
-
-router.prefix('/{{modelname}}');
-
-router.get('/get', controller.get)
-
-router.get('/list', controller.list)
-
-router.get('/add', controller.add)
-
-router.get('/update', controller.update)
-
-router.get('/delete', controller.delete)
 
 module.exports = router
