d2cf72f0a808334252fa6d3aeeaa7d6baaa1facd
丁锐 <dingrui@daojia.com>
2018-03-20 12:33:15 +0800

    fix bug

--- app/controller/<%- ModelName%>Controller.js
+++ app/controller/<%- ModelName%>Controller.js
@@ -1,3 +1,61 @@
 const <%- ModelName%>Model = require('../model/<%- ModelName%>Model')
+const util = require('util')
+
+let controller = {}
+
+controller.get = async (ctx, next) => {
+    const { <%- table.primaryKey%> } = ctx.query
+    return await <%- ModelName%>Model
+        .where({
+            <%- table.primaryKey%>: <%- table.primaryKey%>
+        })
+        .fetch()
+}
+
+controller.list = async (ctx, next) => {
+    return await <%- ModelName%>Model
+        .fetchAll({
+        })
+}
+
+controller.add  = async (ctx, next) => {
+    // TODO: 这里是验证
+
+    const value = {
+        <% for (var i = 0; i < table.columns.length; i++) {
+            if (i == table.columns.length) {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>
+        <% } else {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>,
+        <% }} %>
+    }
+
+    let result = await new <%- ModelName%>Model(value).save()
+    if (result.id <= 0) {
+        throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
+    }
+    return true
+}
+
+controller.update = async (ctx, next) => {
+    // TODO: 这里是验证
+
+
+    let result = await new <%- ModelName%>Model(value).save()
+    if (result.id <= 0) {
+        throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
+    }
+    return true
+}
+
+controller.delete = async (ctx, next) => {
+    const { <%- table.primaryKey%> } = ctx.query
+    await <%- ModelName%>Model
+        .where({
+            <%- table.primaryKey %>: <%- table.primaryKey%>
+        })
+        .destroy()
+    return true
+}
 
 module.exports = controller
