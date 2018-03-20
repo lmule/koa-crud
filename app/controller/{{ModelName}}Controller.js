90dc912af4003f09d5409c6bddab5f9a4dcb606e
丁锐 <dingrui@daojia.com>
2018-03-20 11:00:14 +0800

    ejs

--- app/controller/{{ModelName}}Controller.js
+++ app/controller/{{ModelName}}Controller.js
@@ -1,38 +1,37 @@
-const {{ModelName}}Model = require('../model/{{ModelName}}Model')
+const <%- ModelName %>Model = require('../model/<%- ModelName %>Model')
 const util = require('util')
 
 let controller = {}
 
 controller.get = async (ctx, next) => {
-    const { {{table.primaryKey}} } = ctx.query
-    return await {{ModelName}}Model
+    const { <%- table.primaryKey %> } = ctx.query
+    return await <%- ModelName %>Model
         .where({
-            {{table.primaryKey}}: {{table.primaryKey}}
+            <%- table.primaryKey %>: <%- table.primaryKey %>
         })
         .fetch()
 }
 
 controller.list = async (ctx, next) => {
-    return await {{ModelName}}Model
+    return await <%- ModelName %>Model
         .fetchAll({
         })
 }
 
 controller.add  = async (ctx, next) => {
-    const { {{#each table.columns}}{{#if @last}}{{this}}{{else}}{{this}}, {{/if}}{{/each}} } = ctx.query
-    // 这里是验证
+    const { <%- #each table.columns %><%- #if @last %><%- this %><%- else %><%- this %>, <%- /if %><%- /each %> } = ctx.query
+    // TODO: 这里是验证
 
     const value = {
-        {{#each table.columns}}
-        {{#if @last}}
-        {{this}}: {{this}}
-        {{else}}
-        {{this}}: {{this}},
-        {{/if}}
-        {{/each}}
+        <% for (var i = 0; i < table.columns.length; i++) {
+            if (i == table.columns.length) {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>
+        <% } else {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>,
+        <% }) %>
     }
 
-    let result = await new {{ModelName}}Model(value).save()
+    let result = await new <%- ModelName %>Model(value).save()
     if (result.id <= 0) {
         throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
     }
@@ -40,21 +39,21 @@ controller.add  = async (ctx, next) => {
 }
 
 controller.update = async (ctx, next) => {
-    const { {{#each table.columns}}{{#if @last}}{{this}}{{else}}{{this}}, {{/if}}{{/each}} } = ctx.query
+    const { <%- #each table.columns %><%- #if @last %><%- this %><%- else %><%- this %>, <%- /if %><%- /each %> } = ctx.query
     // TODO: 这里是验证
 
-    {{! 等有空的时候把主键单独拿出来}}
+    <%- ! 等有空的时候把主键单独拿出来 %>
     const value = {
-        {{#each table.columns}}
-        {{#if @last}}
-        {{this}}: {{this}}
-        {{else}}
-        {{this}}: {{this}},
-        {{/if}}
-        {{/each}}
+        <%- #each table.columns %>
+        <%- #if @last %>
+        <%- this %>: <%- this %>
+        <%- else %>
+        <%- this %>: <%- this %>,
+        <%- /if %>
+        <%- /each %>
     }
 
-    let result = await new {{ModelName}}Model(value).save()
+    let result = await new <%- ModelName %>Model(value).save()
     if (result.id <= 0) {
         throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
     }
@@ -62,11 +61,11 @@ controller.update = async (ctx, next) => {
 }
 
 controller.delete = async (ctx, next) => {
-    const { {{table.primaryKey}} } = ctx.query
-    {{! bookshelf在删除的时候好像没有标识是否删除成功（只要不报错就认为是删除成功，然而它并不知道删除了几条）}} 
-    await {{ModelName}}Model
+    const { <%- table.primaryKey %> } = ctx.query
+    <%- ! bookshelf在删除的时候好像没有标识是否删除成功（只要不报错就认为是删除成功，然而它并不知道删除了几条） %> 
+    await <%- ModelName %>Model
         .where({
-            {{table.primaryKey}}: {{table.primaryKey}}
+            <%- table.primaryKey %>: <%- table.primaryKey %>
         })
         .destroy()
     return true
