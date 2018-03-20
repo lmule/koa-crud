844ebe42760c155981d87d7c1fe421c340d8831f
丁锐 <dingrui@daojia.com>
2018-03-20 12:26:45 +0800

    fix bug

--- app/controller/<%- ModelName%>Controller.js
+++ app/controller/<%- ModelName%>Controller.js
@@ -18,4 +18,46 @@ controller.list = async (ctx, next) => {
         })
 }
 
+controller.add  = async (ctx, next) => {
+    const { <%- #each table.columns %><%- #if @last %><%- this %><%- else %><%- this %>, <%- /if %><%- /each %> } = ctx.query
+    // TODO: 这里是验证
+
+    const value = {
+        <% for (var i = 0; i < table.columns.length; i++) {
+            if (i == table.columns.length) {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>
+        <% } else {%>
+            <%- table.columns[i]%>: <%- table.columns[i]%>,
+        <% }} %>
+    }
+
+    let result = await new <%- ModelName %>Model(value).save()
+    if (result.id <= 0) {
+        throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
+    }
+    return true
+}
+
+controller.update = async (ctx, next) => {
+    // TODO: 这里是验证
+
+
+    let result = await new <%- ModelName %>Model(value).save()
+    if (result.id <= 0) {
+        throw new Error(util.format('保存失败，具体数据是:%s', JSON.stringify(value)))
+    }
+    return true
+}
+
+controller.delete = async (ctx, next) => {
+    const { <%- table.primaryKey %> } = ctx.query
+    <%- ! bookshelf在删除的时候好像没有标识是否删除成功（只要不报错就认为是删除成功，然而它并不知道删除了几条） %> 
+    await <%- ModelName %>Model
+        .where({
+            <%- table.primaryKey %>: <%- table.primaryKey %>
+        })
+        .destroy()
+    return true
+}
+
 module.exports = controller
