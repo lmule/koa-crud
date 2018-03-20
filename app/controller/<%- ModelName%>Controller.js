679d9ace64c666a6746056407c7e630ff4163551
丁锐 <dingrui@daojia.com>
2018-03-20 12:49:19 +0800

    test

--- app/controller/<%- ModelName%>Controller.js
+++ app/controller/<%- ModelName%>Controller.js
@@ -40,6 +40,7 @@ controller.add  = async (ctx, next) => {
 controller.update = async (ctx, next) => {
     // TODO: 这里是验证
 
+    <%# 等有空的时候把主键单独拿出来%>
     const value = {
         <% for (var i = 0; i < table.columns.length; i++) {
             if (i == table.columns.length - 1) {%>
@@ -58,6 +59,7 @@ controller.update = async (ctx, next) => {
 
 controller.delete = async (ctx, next) => {
     const { <%- table.primaryKey%> } = ctx.query
+    <%# bookshelf在删除的时候好像没有标识是否删除成功（只要不报错就认为是删除成功，然而它并不知道删除了几条）%>
     await <%- ModelName%>Model
         .where({
             <%- table.primaryKey%>: <%- table.primaryKey%>
